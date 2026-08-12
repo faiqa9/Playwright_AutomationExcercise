2863088b67ea62d04347657600e1428914469c41
Tricentric Labs <tricentriclabs@Tricentrics-MacBook-Pro.local>
2026-08-12 07:20:26 -0700

    Configure authenticated order tests

--- playwright.config.js
+++ playwright.config.js
@@ -38,42 +38,48 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
+projects: [
 
   {
     name: 'setup',
-
     testMatch: /auth\.setup\.spec\.js/,
   },
 
   {
     name: 'chromium-auth',
-
+    testMatch: /order\.spec\.js/,
     use: {
       ...devices['Desktop Chrome'],
-
       storageState: 'playwright/.auth/user.json',
     },
-
     dependencies: ['setup'],
   },
 
-    {
+  {
     name: 'chromium',
+    testIgnore: /order\.spec\.js/,
     use: {
       ...devices['Desktop Chrome'],
     },
   },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+  {
+    name: 'firefox',
+    testIgnore: /order\.spec\.js/,
+    use: {
+      ...devices['Desktop Firefox'],
     },
+  },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+  {
+    name: 'webkit',
+    testIgnore: /order\.spec\.js/,
+    use: {
+      ...devices['Desktop Safari'],
     },
+  },
+
+],
 
     /* Test against mobile viewports. */
     // {
@@ -94,7 +100,7 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  
 
   /* Run your local dev server before starting the tests */
   // webServer: {
